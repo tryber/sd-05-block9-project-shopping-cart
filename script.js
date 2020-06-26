651e411cb77e0824d5d6a5dcdc8b2529e953c92e
Andre V <andrevezi@gmail.com>
2020-06-26 19:30:58 -0300

    Commit 1.1 - Cypress talk to me git add .

--- script.js
+++ script.js
@@ -26,21 +26,21 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 // function cartItemClickListener(event) {
 //   // coloque seu código aqui
 // }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 const apiUrl = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 
@@ -57,7 +57,3 @@ fetch(apiUrl)
       addItem.appendChild(createProductItemElement(product));
     }
   });
-const aux = 'ccparadechorar';
-getSkuFromProductItem(aux);
-event(aux);
-createCartItemElement(aux);
