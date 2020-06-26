6917ca2d5a7bd8662ccbdacc356011db2c2b507c
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-06-26 15:29:11 -0300

    starting to fetch APIs json

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function onload() { };
+window.onload = function onload() {};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -21,7 +21,9 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
+  );
 
   return section;
 }
@@ -41,3 +43,21 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+// ANCHOR 
+
+const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
+
+const myObject = {
+  method: 'GET',
+  headers: { Accept: 'application/json' },
+};
+
+
+const fetchMercadoLivre = () => {
+  fetch(API_URL, myObject)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+};
+
+window.onload = () => fetchMercadoLivre();
