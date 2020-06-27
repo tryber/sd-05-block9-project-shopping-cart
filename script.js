469400d790d832a160403f5bf54a0c28f0f4663b
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-06-26 23:27:12 -0300

    Add requisicao por id na API, criando o carrinho de compras e add o botao e event

--- script.js
+++ script.js
@@ -14,6 +14,28 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function cartShop({ sku }) {
+  fetch(`https://api.mercadolibre.com/items/${sku}`)
+    .then(response => response.json())
+    .then((data) => {
+      const cartItems = document.querySelector('.cart__items');
+      const cartActual = createCartItemElement({
+        sku: data.id,
+        name: data.title,
+        salePrice: data.price,
+      });
+      cartItems.appendChild(cartActual);
+    });
+}
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -23,24 +45,21 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
+  const increaseCartBtt = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  increaseCartBtt.addEventListener('click', () => {
+    cartShop({ sku });
+  });
+  section.appendChild(increaseCartBtt);
   return section;
 }
-
-/* function getSkuFromProductItem(item) {
+/*
+function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
 
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }*/
 
 window.onload = function onload() {
