a29e19f0446b07b58b1b4f91358c2ec369496a00
Preto115 <preto115@gmail.com>
2020-06-27 17:53:53 -0300

    requisito 3 solved

--- script.js
+++ script.js
@@ -24,6 +24,21 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+function cartItemClickListener(event) {
+  const itemExcluido = event.target;
+  // pega carrinho e exclui item clicado
+  const carrinhoDeCompras = document.getElementsByTagName('ol')[0];
+  carrinhoDeCompras.removeChild(itemExcluido.parentNode);
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 function getSkuFromProductItem(item) {
   const itemClicado = item.target.sku;
   // faz uma requição do produto selecionado a API
@@ -41,21 +56,6 @@ function getSkuFromProductItem(item) {
     return itemDoCarrinho.appendChild(img).appendChild(li);
   });
   // return item.querySelector('span.item__sku').innerText;
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-function cartItemClickListener(event) {
-  const itemExcluido = event.target;
-  // pega carrinho e exclui item clicado
-  const carrinhoDeCompras = document.getElementsByTagName('ol')[0];
-  carrinhoDeCompras.removeChild(itemExcluido.parentNode);
 }
 
 window.onload = function onload() {
