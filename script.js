64dc9169f2ae867a77d35904387d4a429f8dfd1b
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-06-27 12:01:13 -0300

    requisito 2 feito

--- script.js
+++ script.js
@@ -15,27 +15,6 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  const addCart = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
-  section.appendChild(addCart);
-  addCart.addEventListener('click', function() {
-    searchProduct(sku);
-  });
-  const sectionElement = document.getElementsByClassName('items')[0];
-  sectionElement.appendChild(section);
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
 function cartItemClickListener(event) {
 
 }
@@ -60,6 +39,27 @@ function searchProduct(sku) {
     const sectionElement = document.getElementsByClassName('cart__items')[0];
     sectionElement.appendChild(createCartItemElement(products));
   });
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const addCart = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  section.appendChild(addCart);
+  addCart.addEventListener('click', function () {
+    searchProduct(sku);
+  });
+  const sectionElement = document.getElementsByClassName('items')[0];
+  sectionElement.appendChild(section);
+  return section;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
 fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
