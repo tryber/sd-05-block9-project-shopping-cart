fb48b4f9862e72300da4235ae5f01d7e11a36324
luiz-avelar <luiz.avelar25@gmail.com>
2020-06-27 19:26:31 -0300

    Requirement 2

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+  
   return section;
 }
 
@@ -42,18 +42,34 @@ function createCartItemElement({ sku, name, salePrice }) {
 
 window.onload = function onload() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then(response => response.json())
-    .then(data => { 
-      return computers = data.results;
+  .then(response => response.json())
+  .then(data => { 
+    return computers = data.results;
+  })
+  .then(() => {
+    computers.forEach((element) => {
+      const {id: sku, title: name, thumbnail: image} = element;
+      const productItem = createProductItemElement({sku, name, image});
+      const itemsSection = document.getElementsByClassName("items")[0];
+      itemsSection.appendChild(productItem);
     })
-    .then(() => {
-      computers.forEach(element => {
-        const {id: sku, title: name, thumbnail: image} = element;
-        const productItem = createProductItemElement({sku, name, image});
-        const itemsSection = document.getElementsByClassName("items")[0];
-        itemsSection.appendChild(productItem);
-      });
-    })
-    .catch(() => console.log('Error'))
-
+  })
+  .then(() => {
+    const addButtons = document.querySelectorAll(".item__add"); //Tem que ser querySelector, pq forEach não itera sobre uma HTML collection
+    addButtons.forEach(element => element.addEventListener('click', getID))
+  })
+  //.catch(() => console.log('Error'))
 };
+
+function getID (event) {
+  const itemID = event.target.parentElement.firstChild.innerHTML;
+  fetch(`https://api.mercadolibre.com/items/${itemID}`)
+  .then(response => response.json())
+  .then(data => {
+    const {id: sku, title: name, price: salePrice} = data;
+    const cartItemElement = createCartItemElement({sku, name, salePrice});
+    const cart = document.getElementsByClassName("cart__items")[0];
+    cart.appendChild(cartItemElement);
+  })
+}
+
