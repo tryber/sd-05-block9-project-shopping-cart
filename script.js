8751b90ec32149952eff0f4fedc5b34dfa59df92
pedrohcalado <pedrocalado22@gmail.com>
2020-06-27 10:40:42 -0300

    Clear cart button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function onload() {
-  document.querySelector('.cart__items').innerHTML = (localStorage.getItem('cart'));
+  cartItems.innerHTML = (localStorage.getItem('cart'));
 };
 
 function createProductImageElement(imageSource) {
@@ -64,20 +64,22 @@ createListOfProducts('computador');
 
 // 2 - add the product to the cart when the button is clicked
 const items = document.querySelector('.items');
-items.addEventListener('click', (event) => {
-  // gets products id
-  const itemId = event.target.parentElement.firstElementChild.innerText;
-
-  // send a request
-  const API_URL_2 = `https://api.mercadolibre.com/items/${itemId}`;
+let itemId;
+let API_URL_2 = `https://api.mercadolibre.com/items/${itemId}`;
   const getObject2 = {
     method: 'GET',
   };
 
+items.addEventListener('click', (event) => {
+  // gets products id
+  itemId = event.target.parentElement.firstElementChild.innerText;
+  API_URL_2 = `https://api.mercadolibre.com/items/${itemId}`;
+  // send a request
+
   fetch(API_URL_2, getObject2)
     .then(response => response.json())
     .then((item) => {
-      document.querySelector('.cart__items').appendChild(createCartItemElement(item));
+      cartItems.appendChild(createCartItemElement(item));
     })
     .then(() => localStorage.setItem('cart', document.querySelector('.cart__items').innerHTML))
     .catch(() => console.log('Error trying to add a product to the cart'));
@@ -87,8 +89,20 @@ items.addEventListener('click', (event) => {
 const cartItems = document.querySelector('.cart__items');
 cartItems.addEventListener('click', () => {
   cartItemClickListener(event).remove();
-  localStorage.setItem('cart', document.querySelector('.cart__items').innerHTML);
+  localStorage.setItem('cart', cartItems.innerHTML);
 });
 
 // 4 - Save to local storage
   // the response is at the function onload at the beginning
+
+// 5 - Create sum of item's prices
+// async function createSum(itemSku) {
+//   let sum = 0;
+// }
+
+// 6 - clear button
+const clearButton = document.querySelector('.empty-cart');
+clearButton.addEventListener('click', () => {
+  cartItems.innerHTML = '';
+  localStorage.setItem('cart', cartItems.innerHTML);
+});
