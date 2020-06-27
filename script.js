0af886b7c5829ed84ac078105723679149c96fae
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-06-27 00:35:49 -0300

    Requisito II - segunda tentativa

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(increaseCartBtt);
   return section;
 }
-/*
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -60,7 +60,15 @@ function getSkuFromProductItem(item) {
 function cartItemClickListener(event) {
   // coloque seu código aqui
 
-}*/
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
 window.onload = function onload() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
