a6607f6a4d7f0fd53f18b814546754f7bbee9d20
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-06-28 16:04:12 -0300

    Criando a função de adicão do produto ao carrinho de compras.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-window.onload = function onload() { };
+window.onload = function onload() {
+  insertProducts();
+};
 
 function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
+  const img = document.createElement("img");
+  img.className = "item__image";
   img.src = imageSource;
   return img;
 }
@@ -15,40 +17,60 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
+  const section = document.createElement("section");
+  section.className = "item";
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createCustomElement("span", "item__sku", sku));
+  section.appendChild(createCustomElement("span", "item__title", name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const btnAddToChart = createCustomElement(
+    "button",
+    "item__add",
+    "Adicionar ao carrinho!"
+  );
+  btnAddToChart.addEventListener("click", () => {
+    getProductDetails({ sku });
+  });
+  section.appendChild(btnAddToChart);
 
   return section;
 }
 
 function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+  return item.querySelector("span.item__sku").innerText;
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  console.log(event);
+  const cart__items = document.querySelector(".cart__items");
+  cart__items.appendChild(createCartItemElement(item));
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
+  const li = document.createElement("li");
+  li.className = "cart__item";
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener("click", cartItemClickListener);
   return li;
 }
 
 // Pegando a lista de computadores
 
-const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+const API_URL = "https://api.mercadolibre.com/sites/MLB/search?q=";
+const PRODUCT_ENDPOINT = "https://api.mercadolibre.com/items/";
 
-function queryProducts(term = 'computador') {
-  console.log(`${API_URL}${term}`);
+function queryProducts(term = "computador") {
   return fetch(`${API_URL}${term}`);
+}
+
+async function getProductDetails({ sku }) {
+  console.log(`${PRODUCT_ENDPOINT}${sku}`);
+  const queryProductById = () => fetch(`${PRODUCT_ENDPOINT}${sku}`);
+  const productDetails = await queryProductById();
+  const { title: name, price: salePrice } = await productDetails.json();
+  const cart__items = document.querySelector(".cart__items");
+  const cart_product = createCartItemElement({ sku, name, salePrice });
+  cart__items.appendChild(cart_product);
 }
 
 async function insertProducts() {
@@ -56,10 +78,18 @@ async function insertProducts() {
   const { results } = await productsResponse.json();
   results.forEach(({ id: sku, title: name, thumbnail: image }) => {
     const item = { sku, name, image };
-    console.log(item);
-    const itemSection = document.querySelector('.items');
+    const itemSection = document.querySelector(".items");
     itemSection.appendChild(createProductItemElement(item));
   });
 }
 
-window.onload = () => insertProducts();
+// window.onload = () => {
+//   insertProducts();
+// };
+const itemButtons = document.getElementsByClassName("item__add");
+// console.log(itemButtons, typeof itemButtons);
+// console.log( Object.entries(itemButtons))
+// itemButtons.forEach(button => {
+//   console.log(button);
+//   button.onclick = (target) => getSkuFromProductItem(target);1
+// })
