6d34f84c352a38bed6e0f1f22380902be639eb18
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-06-28 12:07:14 -0300

    Adicao das funcões emptyCart, saveCart e event na funcao cartItemClickListener

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = function onload() { };
+// Variáveis globais
 
+// Função fornecida
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -7,6 +9,7 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+// Função fornecida
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -14,6 +17,24 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// Salvar o carrinho de compras
+const saveCart = () => {
+  localStorage.setItem('Cart Items', document.querySelector('.cart__items').innerHTML);
+};
+
+// Remover item do carrinho
+function cartItemClickListener(event) {
+  event.target.remove();
+}
+
+// Esvaziar o carrinho
+function emptyCart() {
+  const list = document.getElementsByClassName('cart__items')[0];
+  list.innerHTML = '';
+  saveCart();
+}
+
+// Função fornecida
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -22,20 +43,23 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+// Criando o objeto para ser adicionado ao carrinho de compras
 function cartShop({ sku }) {
   fetch(`https://api.mercadolibre.com/items/${sku}`)
     .then(response => response.json())
     .then((data) => {
-      const cartItems = document.querySelector('.cart__items');
+      const cartItem = document.querySelector('.cart__items')[0];
       const cartActual = createCartItemElement({
         sku: data.id,
         name: data.title,
         salePrice: data.price,
       });
-      cartItems.appendChild(cartActual);
+      cartItem.appendChild(cartActual);
+      return cartActual;
     });
 }
 
+// Função fornecida
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -43,25 +67,16 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  const increaseCartBtt = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
-  increaseCartBtt.addEventListener('click', () => {
-    cartShop({ sku });
-  });
-  section.appendChild(increaseCartBtt);
+  const buttonAdd = section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  buttonAdd.addEventListener('click', () => cartShop(sku));
   return section;
 }
 
 /* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-
 } */
 
+// Fetch para puxar os dados dos produtos através da API
 window.onload = function onload() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
     .then(response => response.json())
