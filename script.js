6da51eeba0580eabca6086e8001eba69c73b3b95
Marylange <marylangess@gmail.com>
2020-06-29 16:28:05 -0300

    fazendo fetch e mapeando elementos sku, image e name com map

--- script.js
+++ script.js
@@ -1,43 +1,75 @@
-window.onload = function onload() { };
 
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
+window.onload = function onload() {
+  const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const fecthApi = () => {
+    const myObject = {
+      method: 'GET',
+      headers: { 'Accept': 'application/json' }
+    };
+    fetch(API_URL, myObject)
+      .then(response => response.json())
+      .then(data => {
+        renderContent(data);
+      });
+  }
 
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-}
+  const mapeiaData = (data) => {
+    const mapeiaAPI = data.results.map(elementos => {
+      return {
+        sku: elementos.id,
+        name: elementos.title,
+        image: elementos.thumbnail,
+      }
+    })
+    console.log(mapeiaAPI);
+    return mapeiaAPI;
+  }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
+  function createProductImageElement(imageSource) {
+    const img = document.createElement('img');
+    img.className = 'item__image';
+    img.src = imageSource;
+    return img;
+  }
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  function createCustomElement(element, className, innerText) {
+    const e = document.createElement(element);
+    e.className = className;
+    e.innerText = innerText;
+    return e;
+  }
 
-  return section;
-}
+  function createProductItemElement({ sku, name, image }) {
+    const section = document.createElement('section');
+    section.className = 'item';
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+    section.appendChild(createCustomElement('span', 'item__sku', sku));
+    section.appendChild(createCustomElement('span', 'item__title', name));
+    section.appendChild(createProductImageElement(image));
+    section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+    return section;
+  }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+  function getSkuFromProductItem(item) {
+    return item.querySelector('span.item__sku').innerText;
+  }
+
+  function cartItemClickListener(event) {
+    // coloque seu código aqui
+  }
+
+  function createCartItemElement({ sku, name, salePrice }) {
+    const li = document.createElement('li');
+    li.className = 'cart__item';
+    li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+    li.addEventListener('click', cartItemClickListener);
+    return li;
+  }
+
+  const renderContent = (data) => {
+    const mapeData = mapeiaData(data);
+  }
+  fecthApi();
+};
+
