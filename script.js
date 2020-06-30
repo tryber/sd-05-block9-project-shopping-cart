6c4798a9d0b7b8e213c1f1b08a927654faa8f4d4
ferrari <luisdiasferrari@gmail.com>
2020-06-30 02:00:36 -0300

    Requirement 2

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-window.onload = function onload() { 
-  fetch("https://api.mercadolibre.com/sites/MLB/search?q=computador")
-    .then(response => response.json())
-    .then((data) => {
-      data.results.forEach((item) => {
-        const product = createProductItemElement({
-          sku: item.id,
-          name: item.title,
-          image: item.thumbnail,
-          })
-          document.querySelector('.items').appendChild(product);
-        })      
-    })  
-};  
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -39,18 +24,52 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//
+// }
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+function criarObjetoCar(sku) {
+  fetch(`https://api.mercadolibre.com/items/${sku}`)
+  .then(response => response.json())
+  .then((data) => {
+    const productAdd = {
+      sku: data.id,
+      name: data.title,
+      salePrice: data.price,
+    };
+    const cartPai = document.getElementsByClassName('cart__items')[0];
+    cartPai.appendChild(createCartItemElement(productAdd));
+  });
+}
+
+window.onload = function onload() {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then(response => response.json())
+    .then((data) => {
+      data.results.forEach((item) => {
+        const product = createProductItemElement({
+          sku: item.id,
+          name: item.title,
+          image: item.thumbnail,
+        });
+        document.querySelector('.items').appendChild(product);
+      });
+    })
+    .then(() => {
+      document.querySelectorAll('.item__add').forEach(addItem => addItem
+      .addEventListener('click', () => criarObjetoCar(addItem
+      .parentElement.querySelector('span.item__sku').innerText)));
+    });
+};
