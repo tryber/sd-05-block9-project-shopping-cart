1bd351e6154ebb87edd6e45c3317088290e4ad09
marinabrcls <barcelos.marina@gmail.com>
2020-07-02 12:45:08 -0300

    algumas alteraçoes sugeridas

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // requisito 4. função que gera o carrinho
-/* const getCart = () => {
-  const newCart = JSON.parse(localStorage.getItem('cart'));
+const getCart = () => {
+  const newCart = JSON.parse(localStorage.getItem('myCart'));
   return newCart;
-}; s
+};
 let cart = getCart();
-*/
 
 // requisito 4. função que salva itens do carrinho
-/* const saveCart = () => {
-  localStorage.setItem('cart', JSON.stringify(cart));
-}; */
+const saveCart = () => {
+  localStorage.setItem('myCart', JSON.stringify(cart));
+};
 
 // função fornecida
 function createProductImageElement(imageSource) {
@@ -26,27 +25,26 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
-/*
+
 // requisito 3. remove um item do carrinho quando clicado nele
 function cartItemClickListener(event) {
   event.target.remove();
+  const remakeCart = cart.filter(({ id }) => `${id}` !== event.target.id);
+  cart = remakeCart;
   saveCart();
   sumTotal();
 }
 
- */// função fornecida mas fiz alterações
+// função fornecida mas fiz alterações
 function createCartItemElement({ name, salePrice, id }) {
   const li = document.createElement('li');
-  const span = document.createElement('span');
-  span.className = 'span__item';
   li.className = 'cart__item';
   li.id = id;
   li.innerText = `SKU: ${id} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
-  span.appendChild(li);
-  return span;
+  return li;
 }
-/*
+
 // requisito 5. faz a soma total
 const sumTotal = async () => {
   const total = document.querySelector('.total-price');
@@ -56,30 +54,30 @@ const sumTotal = async () => {
 // requisito 4. carrega o carrinho a partir do local storage
 const loadCart = () => {
   getCart()
-  .map(products => createCartItemElement(products))
-  .forEach((singleProduct) => {
-    document.getElementsByClassName('.cart__items').appendChild(singleProduct);
-  });
+    .map(products => createCartItemElement(products))
+    .forEach((singleProduct) => {
+      document.getElementsByClassName('.cart__items').appendChild(singleProduct);
+    });
   sumTotal();
 };
-*/
 
 // requisito 2. função que adiciona ao carrinho
-async function addToCart(sku) {
+async function addToCart({ sku }) {
+  console.log(sku);
   await fetch(`https://api.mercadolibre.com/items/${sku}`)
     .then(response => response.json())
     .then((data) => {
       const newCartItem = {
-        sku: data.id,
         name: data.title,
         salePrice: data.price,
-        id: Math.floor(Math.random() * 9999999),
+        id: data.sku,
       };
+      cart.push(newCartItem);
       const cartItems = document.getElementsByClassName('cart__items')[0];
       cartItems.appendChild(createCartItemElement(newCartItem));
     });
-  // saveCart();
-  // sumTotal();
+  saveCart();
+  sumTotal();
 }
 
 // função fornecida mas fiz modificações
@@ -89,16 +87,17 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  const buttonAdd = section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const buttonAdd = section.appendChild(
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
+  );
   buttonAdd.addEventListener('click', () => addToCart({ sku }));
   section.appendChild(buttonAdd);
   return section;
 }
 
-/* function getSkuFromProductItem(item) {
+function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-} */
-/*
+}
 
 // requisito 6. botão que limpa o carrinho
 async function clearCart() {
@@ -111,7 +110,6 @@ async function clearCart() {
 
 const clearButton = document.querySelector('.empty-cart');
 clearButton.addEventListener('click', clearCart);
- */
 
 // requisito 1. gerar lista de produtos
 window.onload = function onload() {
@@ -128,5 +126,5 @@ window.onload = function onload() {
       });
     })
     .catch(() => console.log('Error: Could not load the API'));
-  // loadCart();
+  loadCart();
 };
