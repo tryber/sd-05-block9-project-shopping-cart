a7cd5fca750c0087f51e9522f09b5eb4fc8af6df
Lucas Staroscky <LucasStaroscky@MBP-de-Lucas.lan>
2020-07-03 11:51:39 -0300

    second requirement completed

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-window.onload = function onload() { };
+window.onload = function onload() {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch("https://api.mercadolibre.com/sites/MLB/search?q=computador", myObject)
+    .then(responde => responde.json())
+    .then(data => {
+      data.results.forEach(element => {
+        const createProduct = createProductItemElement({
+          sku: element.id,
+          name: element.title,
+          image: element.thumbnail
+        });
+        document.querySelectorAll(".items")[0].appendChild(createProduct);
+      });
+    })
+};
+
+function cartItemClickListener(event) {
+  event.target.remove();
+}
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -17,21 +39,19 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  const btnAddCart = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  btnAddCart.addEventListener('click', () => {
+    addToCArt({ sku })
+  });
+  section.appendChild(btnAddCart);
   return section;
 }
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -40,4 +60,17 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+// function to add item to cart
+async function addToCArt({ sku }) {
+  const response = await fetch(`https://api.mercadolibre.com/items/${sku}`)
+  const data = await response.json()
+  const cart_item = document.querySelectorAll('.cart__items')[0];
+  const addNewCArtItem = createCartItemElement({
+    sku: data.id,
+    name: data.title,
+    salePrice: data.price,
+  })
+  cart_item.appendChild(addNewCArtItem);
 }
---
+++ test.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const scriptFunctionsTest = require('./script');
+const { hasUncaughtExceptionCaptureCallback } = require('process');
+const { promises } = require('fs');
+
+
+const testing = (parameter) => {
+  describe(`${parameter}`, () => {
+    test("true assert!", () => {
+      expect(typeof parameter).toBe('function');
+    })()
+  })
+}
+
+testing(scriptFunctionsTest)
+
