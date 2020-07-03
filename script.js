1a745ae81e7cc83d11a575a78071b6ac99bdc3ae
Marylange <marylangess@gmail.com>
2020-07-03 18:09:06 -0300

    requisito 3

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+const removeElementosComClick = () => {
+  const elementoDoCarrinho = document.querySelector('.cart__item');
+  elementoDoCarrinho.addEventListener('click', cartItemClickListener);
+}
+
 function cartItemClickListener(event) {
+  const li = document.querySelector('li');
+  const elementoQueGerouOEvento = event.target;
+  li.remove() = elementoQueGerouOEvento;
 
 }
+
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
