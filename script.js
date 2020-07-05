0767afedaaf34e5aea250d8e36191fac0c5cda87
pauloricardoz <trybe.przt@gmail.com>
2020-07-05 01:55:08 -0300

    localStorage done codeclimate 1

--- script.js
+++ script.js
@@ -79,9 +79,9 @@ function createProductItemElement({ id, title, thumbnail }) {
 window.onload = function onload() {
   const ol = document.getElementsByClassName('cart__items')[0];
   ol.innerHTML = localStorage.getItem('cartNow');
-  if (ol.children.length > 0){
-    for (let i = 0; i < ol.children.length;   i += 1) {
-      ol.children[i].addEventListener ('click', cartItemClickListener);
+  if (ol.children.length > 0) {
+    for (let i = 0; i < ol.children.length; i += 1) {
+      ol.children[i].addEventListener('click', cartItemClickListener);
     }
   }
   const sectionProducts = document.getElementsByClassName('items')[0];
