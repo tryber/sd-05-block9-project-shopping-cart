a44bcc229ca94d61d1246a253bfe98659bdb234e
luca <luucadecastro@gmail.com>
2020-07-06 10:30:12 -0300

    alteração

--- script.js
+++ script.js
@@ -24,32 +24,28 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  // return item.querySelector('span.item__sku').innerText;
-  return item.target.sku;
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', getSkuFromProductItem);
-  return li;
-}
-
 function cartItemClickListener(event) {
-  const itemClicado = getSkuFromProductItem(event);
-  fetch(`https://api.mercadolibre.com/items/${itemClicado}`)
-  .then(response => response.json())
-  .then(function (produtoAdicionado) {
-    const { id: sku, title: name, thumbnail: image, price: salePrice } = produtoAdicionado;
-    const carrinho = document.getElementsByTagName('ol')[0];
-    const ol = createCartItemElement({ sku, name, salePrice });
-    const img = document.createElement('div');
-    img.appendChild(createProductImageElement(image));
-    // anexando o produto escolhido dentro do carrinho
-    return carrinho.appendChild(img).appendChild(ol);
-  });
+  const itemExcluido = event.target;
+  const carrinhoDeCompras = document.getElementsByTagName('ol')[0];
+}
+
+  function createCartItemElement({ sku, name, salePrice }) {
+    const li = document.createElement('li');
+    li.className = 'cart__item';
+    li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+    li.addEventListener('click', cartItemClickListener);
+    return li;
+  }
+
+  function getSkuFromProductItem(item) {
+    const itemClicado = item.target.sku;
+    fetch(`https://api.mercadolibre.com/items/{$itemClicado}`)
+    .then(responde => responde.json())
+    .then(function (produtoAdicionado) {
+      const { id: sku, title: name, price: salePrice } = produtoAdicionado;
+      const itemDoCarrinho = document.getElementsByTagName('ol')[0];
+      const li = createCartItemElement({ sku, name, salePRice });
+    })
 }
 
 window.onload = function onload() {
