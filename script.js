55a5c27c876bb3ff1a0b7c09a8f4fa7e687e6fe7
luca <luucadecastro@gmail.com>
2020-07-06 10:19:40 -0300

    codeclimate 4

--- script.js
+++ script.js
@@ -29,30 +29,27 @@ function getSkuFromProductItem(item) {
   return item.target.sku;
 }
 
-function cartItemClickListener(event) {
-  const itemClicado = getSkuFromProductItem(event);
-  // faz uma requição do produto selecionado a API
-  fetch(`https://api.mercadolibre.com/items/${itemClicado}`)
-  .then(response => response.json())
-  .then(function (produtoAdicionado) {
-    // quebrando em informacoes do produto o objeto convertido
-    const { id: sku, title: name, thumbnail: image, price: salePrice } = produtoAdicionado;
-    const carrinho = document.getElementsByTagName('ol')[0];
-    // cria as informacoes que serao exibidas
-    const ol = createCartItemElement({ sku, name, salePrice });
-    const img = document.createElement('div');
-    img.appendChild(createProductImageElement(image));
-    // anexando o produto escolhido dentro do carrinho
-    return carrinho.appendChild(img).appendChild(ol);
-  });
-}
-
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', getSkuFromProductItem);
   return li;
+}
+
+function cartItemClickListener(event) {
+  const itemClicado = getSkuFromProductItem(event);
+  fetch(`https://api.mercadolibre.com/items/${itemClicado}`)
+  .then(response => response.json())
+  .then(function (produtoAdicionado) {
+    const { id: sku, title: name, thumbnail: image, price: salePrice } = produtoAdicionado;
+    const carrinho = document.getElementsByTagName('ol')[0];
+    const ol = createCartItemElement({ sku, name, salePrice });
+    const img = document.createElement('div');
+    img.appendChild(createProductImageElement(image));
+    // anexando o produto escolhido dentro do carrinho
+    return carrinho.appendChild(img).appendChild(ol);
+  });
 }
 
 window.onload = function onload() {
