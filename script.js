5a6e331624f12846e74a8934fe3bd9036419f9f7
luialbeto <araujo.script@gmail.com>
2020-07-06 01:15:03 -0300

    somaCarrinho

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+function totalPrice(price) {
+  const valorSoma = document.querySelector('.total-price');
+  valorSoma.innerHTML = parseFloat(valorSoma.innerHTML) + price;
+}
+
 function atualizaStorage() {
+  const price = document.querySelector('.total-price');
   const acessaItem = document.querySelector('.cart__items');
   localStorage.setItem('carrinho', acessaItem.innerHTML);
+  localStorage.setItem('Soma Compra', price.innerHTML);
 }
 
 function createProductImageElement(imageSource) {
@@ -13,6 +20,10 @@ function createProductImageElement(imageSource) {
 function cartItemClickListener(event) {
   // coloque seu código aqui
   const pai = event.target.parentNode;
+  let price = event.target.innerHTML;
+  price = parseFloat(price.substr(price.indexOf('PRICE: $') + 8));
+  console.log(price);
+  totalPrice(-price);
   pai.removeChild(event.target);
   // event.target.parentNode.removeChild(event.target);
   atualizaStorage();
@@ -23,6 +34,7 @@ function createCartItemElement({ id, title, price }) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
   li.addEventListener('click', cartItemClickListener);
+  totalPrice(price);
   return li;
 }
 
@@ -50,7 +62,6 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!', sku));
-
   return section;
 }
 
@@ -60,12 +71,17 @@ function getSkuFromProductItem(item) {
 
 window.onload = function onload() {
   const acessaItem = document.querySelector('.cart__items');
+  const prices = document.querySelector('.total-price');
   const botao = document.querySelector('.empty-cart');
   botao.addEventListener('click', () => {
+    prices.innerHTML = 0;
     acessaItem.innerHTML = null;
     atualizaStorage();
   }); // htmlelement.addEventListener('evento que estou esperando', função que devo realizar)
   acessaItem.innerHTML = localStorage.getItem('carrinho');
+  if (localStorage.getItem('Soma Compra') !== null) {
+    prices.innerHTML = localStorage.getItem('Soma Compra');
+  }
   if (acessaItem.children.length > 0) {
     for (let i = 0; i < acessaItem.children.length; i += 1) {
       acessaItem.children[i].addEventListener('click', cartItemClickListener);
