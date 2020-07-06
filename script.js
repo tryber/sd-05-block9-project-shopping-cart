c816cb2e3d87699371505783edb655a933b9c05a
Sidnei Ramos <sidneiramos02@gmail.com>
2020-07-05 23:54:14 -0300

    Initial Commit

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -17,18 +15,19 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  const botao = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  botao.addEventListener('click',() => addToCart({sku}))
+  section.appendChild(botao)
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
@@ -41,3 +40,24 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+async function addToCart ({sku}) {
+  fetch (`https://api.mercadolibre.com/items/${sku}`)
+  .then (response => response.json())
+  .then (data => {
+    const ident = {sku: data.id, name: data.title, salePrice: data.price}
+    const cartId = createCartItemElement(ident)
+    document.querySelector('.cart__items').appendChild(cartId)
+  })
+} 
+
+window.onload = function onload() { 
+  fetch ('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  .then (response => response.json())
+  .then (data => data.results.forEach(element => {
+    const product = createProductItemElement({sku: element.id, name: element.title, image: element.thumbnail})
+    document.querySelector('.items').appendChild(product)
+  }));
+
+
+};
