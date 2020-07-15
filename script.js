fd694c27a372097a3b1180bdfb32e190647c9960
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-14 22:14:15 -0300

    sum prices function ajusted again...

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let price = 0;
 
 async function sumPrices(valor) {
   price += valor;
-  document.querySelector('.total-price').innerText = Math.round(price);
+  document.querySelector('.total-price').innerText = Math.round(price * 100) / 100;
 }
 
 function cartItemClickListener(event) {
@@ -10,7 +10,7 @@ function cartItemClickListener(event) {
   remove.parentNode.removeChild(remove);
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ sku, name, salePrice }) { 
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
