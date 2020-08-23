120240eb74596a696467bae0cad68a097ce76c5c
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-08-23 04:54:58 -0300

    CC reclamando dos meus comments de guia

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
-  event.target.remove(); 
-  //Perdi tempo achando que quando clicava adicionar ao carrinho removia da lista mas é se clicar no texto remove do carrinho.
+  event.target.remove();
+
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
