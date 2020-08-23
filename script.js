e649f0c9eb130547803946b3a656d05bd47db283
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-08-23 04:58:02 -0300

    CC

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ function getSkuFromProductItem(item) {
 function cartItemClickListener(event) {
   // coloque seu código aqui
   event.target.remove();
-
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
