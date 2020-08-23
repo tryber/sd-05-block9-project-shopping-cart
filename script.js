e964110e55058c8f22aa22c08e4b54ec9b39fe56
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-08-23 02:31:05 -0300

    CC removendo trailing spaces

--- script.js
+++ script.js
@@ -19,8 +19,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  
-  return section;
+    return section;
 }
 
 function getSkuFromProductItem(item) {
